7b733e5285826b51b9e349e98fff3353c4017062
Kilian Agboton <agbotonstefane@gmail.com>
2023-01-23 01:47:45 -0500

    user input - TypeOf function - type casting - constant

--- workspace/main.go
+++ workspace/main.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+)
+
+const PI float64 = 3.14 //global constant
 
 func main() {
 	var hello string = "Hello"
@@ -19,4 +25,54 @@ func main() {
 
 	//print format %d
 	fmt.Printf("Welcome to grade %d", grade)
+
+	//short variable declaration
+	name := "toto"
+	fmt.Println(name)
+
+	//variable scope
+	city := "Kingston"
+	fmt.Println(city)
+	{
+		country := "Canada"
+		fmt.Println(country)
+
+	}
+	//cant access country variable here
+
+	//User input
+	//var nom string
+	//fmt.Println("Met ton nom:")
+	//fmt.Scanf("%s", &nom)
+	//fmt.Println("Le nom saisi est:", nom)
+
+	//Multiple input
+	var a string
+	var b int
+	fmt.Print("Add a string and a number")
+	count, err := fmt.Scanf("%s %d", &a, &b)
+	fmt.Println("count:", count)
+	fmt.Println("error:", err)
+	fmt.Println("a:", a)
+	fmt.Println("b:", b)
+
+	//Type of a variable with %T
+	var grades int = 42
+	fmt.Printf("variable grades = %v is of type %T", grades, grades)
+
+	//Type of a variable with TypeOf function from reflect package
+	fmt.Printf("Type: %v \n", reflect.TypeOf(1000))
+
+	//Type casting - convert types
+	var i int = 12
+	var f float64 = float64(i) //convert int to float
+	fmt.Println(f)
+
+	var s = strconv.Itoa(i) //convert int to string
+	fmt.Printf("%q", s)
+
+	//Constants - type is optional
+	const myConst string = "my string constant" //typed constant
+	const ageee = 16                            //untyped constant
+	const t_name, t_age = "Toto", 15            //untyped constant
 }
